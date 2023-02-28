d7e2c2698c9d385412d687b49e2a5df521d727a5
Brandon Newell <fullaware@users.noreply.github.com>
2023-02-28 15:56:56 -0500

    updated

--- web.py
+++ web.py
@@ -12,4 +12,4 @@ def hello_user(username):
     return 'Hello %s' % escape(username)
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=False)
